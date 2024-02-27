05efea81c6c590bcea724148d9a728336a1c52ff
y7pWuXAq <mylullaby_@naver.com>
2024-02-27 11:18:37 +0900

    C++ 4일차

---
+++ day05/Project23/Project23/tast_03_Destructor.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person
+{
+private :
+	char* name;
+	int age;
+public :
+	Person(const char* myname, int myage)
+	{
+		int len = strlen(myname) + 1; // strlen() : 문자열의 길이를 구하는 함수, strlen은 문자의 갯수만 문자열!은 +1 필요
+		name = new char[len]; // new : 힙영역에 메모리 재할당
+		cout << "char 크기 : " << sizeof(char) << endl;
+		strcpy(name, myname);
+		age = myage;
+	}
+	void ShowPersonInfo() const
+	{
+		cout << "이름 : " << name << endl;
+		cout << "나이 : " << age << endl;
+	}
+	~Person()		// 소멸자 : 호출 안해도 자동으로 호출
+	{
+		delete[]name;
+		cout << "called destructor!" << endl;
+	}
+};
+
+int main(void)
+{
+	Person man1("lee dong woo", 29);
+	Person man2("Jang dong gun", 41);
+	man1.ShowPersonInfo();
+	man2.ShowPersonInfo();
+	return 0;
+}
---
+++ day05/Project23/Project23/test_04_new.cpp
@@ -0,0 +1,20 @@
+﻿/*
+메모리 동적 할당 : new 연산자 사용
+프로그램 실행 중 메모리를 할당 받는다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "int size : " << sizeof(int) << endl;
+	int* pi = new int;		// new 연산자를 통해서 heao 영역에 int(4byten) 크기만큼 메모리공간을 할당 받음
+			// 할당 받은 메모리의 주소를 리턴 시켜준다.
+	*pi = 10;
+	cout << "PI가 가리키는 곳에 저장 된 값 : " << *pi << endl;
+	
+	delete pi;		// 동적 할당받은 메모리 공간을 반환한다.
+
+	return 0;
+}
