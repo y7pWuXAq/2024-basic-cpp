1fb9157a8bbd147a70c10a9416cb89edb235ac79
y7pWuXAq <mylullaby_@naver.com>
2024-03-07 17:30:18 +0900

    C++ 11일차

---
+++ day11/Project4/Project4/02_Grade01.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+class Grade
+{
+private:
+	char* name;
+	int kor;
+	int eng;
+	int math;
+
+public:
+	Grade(const char* name, int kor, int eng, int math)
+	{
+		this->name = new char[strlen(name) + 1];
+		strcpy(this->name, name);
+
+		this->kor = kor;
+		this->eng = eng;
+		this->math = math;
+	}
+	~Grade()
+	{
+		printf("소멸자 호출\n");
+		delete[] name;
+	}
+	int SUM()
+	{
+		int sum = kor + eng + math;
+		return sum;
+	}
+	double AVR()
+	{
+		double avr = SUM() / 3.0;
+		return avr;
+	}
+	int getKor()
+	{
+		return kor;
+	}
+	int getEng()
+	{
+		return eng;
+	}
+	int getMath()
+	{
+		return math;
+	}
+
+};
+
+int main()
+{
+	// Grade* gradeAry[3];	// 객체 포인터 배열
+	Grade gradeAry[3];
+
+	char name[20];
+	int inKor;
+	int inEng;
+	int inMath;
+
+	// 1. 입력을 받는다.
+	for (int i = 0; i < 3; i++)
+	{
+		cout << " 이름 : ";
+		cin >> name;
+		cout << " 국어점수 : ";
+		cin >> inKor;
+		cout << " 영어점수 : ";
+		cin >> inEng;
+		cout << " 수학점수 : ";
+		cin >> inMath;
+
+		// cout << name << inKor << inEng << inMath << endl;
+
+	// 2. 객체생성 : 5개의 객체를 생성, 객체배열 or 객체 포인터 배열
+		gradeAry[i] = new Grade(name, inKor, inEng, inMath);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		// cout << "총점 : " << gradeAry[i]->SUM() << "  " << "평균 : " << gradeAry[i]->AVR() << endl;
+		printf("%d번째 학생 총점 : %d, 평균 : %.1lf\n", i + 1, gradeAry[i]->SUM(), gradeAry[i]->AVR());
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << gradeAry[i]->getKor() << endl;
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		printf("객체 할당 소멸자 호출\n");
+		delete gradeAry[i];
+	}
+
+	return 0;
+}
---
+++ day11/Project4/Project4/03_Grade02.cpp
@@ -0,0 +1,116 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Grade
+{
+private:
+	char* name;
+	int kor;
+	int english;
+	int math;
+public:
+	Grade(const char* myname, int mykor, int myenglish, int mymath)
+	{
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+		kor = mykor;
+		math = mymath;
+		english = myenglish;
+	}
+	Grade()
+	{
+		name = NULL;
+		kor = 0;
+		math = 0;
+		english = 0;
+	}
+	~Grade()
+	{
+		cout << "called destructor!" << endl;
+	}
+	void SetGradeInfo(char* myname, int mykor, int mymath, int myenglish)
+	{
+		name = myname;
+		kor = mykor;
+		math = mymath;
+		english = myenglish;
+	}
+	int SUM() const
+	{
+		return (kor + math + english);
+	}
+	double AVR() const
+	{
+		return SUM() / 3.0;
+	}
+	int getKor()
+	{
+		return kor;
+	}
+	int getEng()
+	{
+		return english;
+	}
+	int getMath()
+	{
+		return math;
+	}
+};
+
+int main()
+{
+	Grade gradeAry[3];
+
+	char name[20];
+	int inKor = 0;
+	int inEng = 0;
+	int inMath = 0;
+	int num = 0;
+
+	cout << "성적 처리 할 학생 수 : ";
+	cin >> num;
+
+	// vector<Grade*> gradeArt(num);
+	// vector<Grade> gradeArt(num);
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << " 이름 : ";
+		cin >> name;
+		cout << " 국어점수 : ";
+		cin >> inKor;
+		cout << " 영어점수 : ";
+		cin >> inEng;
+		cout << " 수학점수 : ";
+		cin >> inMath;
+		gradeAry[i].SetGradeInfo(name, inKor, inEng, inMath);
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		// cout << "총점 : " << gradeAry[i]->SUM() << "  " << "평균 : " << gradeAry[i]->AVR() << endl;
+		printf("%d번째 학생 총점 : %d, 평균 : %.1lf\n", i + 1, gradeAry[i].SUM(), gradeAry[i].AVR());
+	}
+
+	int sumKor = 0;
+	int sumEng = 0;
+	int sumMath = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		// sumKor +=gradeAry[i].getKor();
+		sumKor = sumKor + gradeAry[i].getKor();
+		sumEng = sumEng + gradeAry[i].getEng();
+		sumMath = sumMath + gradeAry[i].getMath();
+	}
+
+	//float(size(gradeAry)) = 배열의 크기를 실수로 변환
+	printf("국어 총점 : %d, 국어 평균 : %.1lf\n", sumKor, sumKor/float(size(gradeAry)));
+	printf("영어 총점 : %d, 영어 평균 : %.1lf\n", sumEng, sumEng/ float(size(gradeAry)));
+	printf("수학 총점 : %d, 수학 평균 : %.1lf\n", sumMath, sumMath/ float(size(gradeAry)));
+
+	return 0;
+}
